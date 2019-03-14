8c3b1aa824bd1f15c805d085a8223f56ea0e9d11
Sandy Wyper <sandywyper@gmail.com>
2019-03-14 15:54:04 +0000

    lazy loading on image carousel

--- javascript/gwenna.js
+++ javascript/gwenna.js
@@ -1,7 +1,18 @@
 // 
 const headerLarge = document.querySelector('#home');
 const headerSlim = document.querySelector('#slim-nav');
+const menuSec = document.querySelector('.menu');
 const slides = document.getElementsByClassName('slides');
+
+
+
+// const lazyImages = document.getElementsByClassName('lazy');
+
+const lazyImages = [].slice.call(document.getElementsByClassName("lazy"));
+let loaded = false;
+
+
+
 // initialise slide show variables
 let slideIndex = 1;
 let autoPlay;
@@ -14,6 +25,8 @@ function readyPage() {
     document.querySelector('#open-menu').addEventListener("click", openSideMenu);
     document.querySelector('#close-menu').addEventListener("click", closeSideMenu);
 
+
+
     //parallax plugin
     var rellax = new Rellax('.rellax', {
         speed: -4.8,
@@ -21,10 +34,8 @@ function readyPage() {
         vertical: true,
         horizontal: false,
         wrapper: null
-            });
+    });
 
-    //play image carousel
-    playSlides();
 }
 
 function openSideMenu() {
@@ -53,6 +64,18 @@ function checkScroll(e) {
         headerLarge.classList.toggle('reveal');
         headerSlim.classList.toggle('reveal');
     }
+
+    //lazy loading the image carousel
+    if (scrolls > menuSec.scrollHeight && loaded === false) {
+        // console.log("triggered");
+        loaded = true;
+        lazyImages.forEach((lazyImage) => {
+            lazyImage.src = lazyImage.dataset.src;
+        });
+        //start playing the image carousel
+        playSlides();
+    }
+
 }
 
 //scroll event throttler to stop the browser from overworking
@@ -65,8 +88,9 @@ function scrollThrottler() {
             scrollTimeout = null;
             checkScroll();
 
-            // The checkScroll will execute at a rate of 15fps
-        }, 20);
+
+            // The checkScroll will execute at a rate of 
+        }, 200);
     }
 }
 
